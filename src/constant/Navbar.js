32bb4e0e8f3bd062916233420abd2a66d8a0afda
shafidark <safi29317@gmail.com>
2023-01-22 18:14:52 +0600

    first commit

---
+++ src/constant/Navbar.js
@@ -0,0 +1,38 @@
+export const Nav_data = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+    dropdown: false,
+  },
+  {
+    id: 1,
+    title: "Services",
+    link: "/",
+    dropdown: false,
+  },
+  {
+    id: 1,
+    title: "About",
+    link: "/",
+    dropdown: false,
+  },
+  {
+    id: 1,
+    title: "Work",
+    link: "/",
+    dropdown: false,
+  },
+  {
+    id: 1,
+    title: "Case study",
+    link: "/",
+    dropdown: [
+      {
+        id: 1,
+        title: "Work",
+        link: "/",
+      },
+    ],
+  },
+];
