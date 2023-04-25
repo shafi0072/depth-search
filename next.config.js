a44c8087d71c248f1a5d08761ce7a16e9ea9e6e2
shafi0072 <safi29317@gmail.com>
2023-04-25 21:31:06 +0600

    shafi

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  optimizeFonts: false,
-  images: {
-    loader: 'akamai',
-    path: '',
-  }
 }
 
-module.exports = nextConfig
 
+
+module.exports = nextConfig
